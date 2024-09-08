c32f2242093d2f8f473b528c77e3c57190596363
AdrianGerman <adrian.german1019@gmail.com>
2024-09-08 09:34:36 -0600

    creating frontend structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,33 @@
+import React from "react"
 import "./App.css"
 
 function App() {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const [file] = event.target.files ?? []
+    console.log(file)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log("TODO")
+  }
   return (
     <>
-      <h1>Challenge: Upload CSV + Search</h1>
+      <main>
+        <h4>Challenge: Upload CSV + Search</h4>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <input
+              onChange={handleInputChange}
+              name="file"
+              type="file"
+              accept=".csv"
+            />
+          </label>
+
+          <button>Subir archivo</button>
+        </form>
+      </main>
     </>
   )
 }
